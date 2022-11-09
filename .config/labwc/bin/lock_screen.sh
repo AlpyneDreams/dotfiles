6ee63f16f5f32119ebf9cea9bb157d608ae893ed
Alpyne <alpyne.dreams@gmail.com>
2022-11-08 23:14:14 -0800

    Add labwc configs

---
+++ .config/labwc/bin/lock_screen.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+swaylock \
+    --image ~/Wallpapers/lockscreen.png \
+    --indicator-radius 128 \
+    --indicator-thickness 24 \
+    --inside-color 91bebb \
+    --inside-caps-lock-color 91bebb \
+    --inside-clear-color 91bebb \
+    --inside-ver-color 91bebb \
+    --inside-wrong-color 91bebb \
+    --ring-color fcfdf5 \
+    --ring-caps-lock-color fcfdf5 \
+    --ring-clear-color fcfdf5 \
+    --ring-ver-color fcfdf5 \
+    --ring-wrong-color fcfdf5 \
+    -n \
+    -f \
+    --text-color fcfdf5 \
+    --text-clear-color fcfdf5 \
+    --text-caps-lock-color fcfdf5 \
+    --text-ver-color fcfdf5 \
+    --text-wrong-color fcfdf5 \
+    --separator-color 00000000 \
+    --key-hl-color feb2bf \
+    --bs-hl-color 95e3ec
+
---
+++ .config/labwc/bin/video.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wf-recorder --audio -g "$(slurp)"
+
